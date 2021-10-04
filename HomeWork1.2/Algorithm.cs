899095e5f6e827db8489679aa355707b1dddbe40
dan <danilsocol-2002@mail.ru>
2021-10-04 05:23:20 +0500

    1

---
+++ HomeWork1.2/Algorithm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Numerics;
+using System.Collections;
+
+namespace HomeWork1._2
+{
+     public class Algorithm
+    {
+        public static void Sort(int[] arr, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(arr, n, i);
+            }
+                
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+        }
+
+
+        static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1; 
+            int r = 2 * i + 2; 
+
+            if (l < n && arr[l] > arr[largest])
+            {
+                largest = l;
+            }
+
+            if (r < n && arr[r] > arr[largest])
+            {
+                largest = r;
+            }
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                Heapify(arr, n, largest);
+            }
+        }
+
+        public static void Factorial(int n, int count)
+        {
+            int result = 1;
+            for (int i = 0; i < count; i++)
+            {
+                result *= n;
+            }
+        }
+
+        static void Swap(ref int value1, ref int value2)
+        {
+            var temp = value1;
+            value1 = value2;
+            value2 = temp;
+        }
+
+        static int GetNextStep(int s)
+        {
+            s = s * 1000 / 1247;
+            return s > 1 ? s : 1;
+        }
+
+        public static void CombSort(int[] array, int count)
+        {
+            var arrayLength = count;
+            var currentStep = count - 1;
+
+            while (currentStep > 1)
+            {
+                for (int i = 0; i + currentStep < array.Length; i++)
+                {
+                    if (array[i] > array[i + currentStep])
+                    {
+                        Swap(ref array[i], ref array[i + currentStep]);
+                    }
+                }
+
+                currentStep = GetNextStep(currentStep);
+            }
+
+            for (var i = 1; i < arrayLength; i++)
+            {
+                var swapFlag = false;
+                for (var j = 0; j < arrayLength - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    { 
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapFlag = true;
+                    }
+                }
+
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void GnomeSort(int[] inArray, int count)
+        {
+            int i = 1;
+            int j = 2;
+            while (i < count)
+            {
+                if (inArray[i - 1] < inArray[i])
+                {
+                    i = j;
+                    j += 1;
+                }
+                else
+                {
+                    GnomeSwap(inArray, i - 1, i);
+                    i -= 1;
+                    if (i == 0)
+                    {
+                        i = j;
+                        j += 1;
+                    }
+                }
+            }
+        }
+        static void GnomeSwap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+}
---
+++ HomeWork1.2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Numerics;
+using System.Collections;
+
+
+namespace HomeWork1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 9);
+
+            int[] array = new int[1000];
+            CreateArray(array);
+
+            OutputDataArray(array, Algorithm.GnomeSort, "GnomeSort");
+           // OutputDataArray(array, Algorithm.Sort, "HeapSort");
+           // OutputDataArray(array, Algorithm.CombSort, "CombSort");
+           // OutputDataFunction(num, Algorithm.Factorial, "Factorial");
+        }
+        public static void CreateArray(int[] array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 200; i++)
+            {
+                array[i] = rnd.Next(1, 9);
+            }
+        }
+        static void OutputDataFunction(int num, Action<int,int> f, string nameAction)
+        {
+            List<int> time = new List<int>();
+
+            using (StreamWriter file = new StreamWriter("Time.txt", true))
+            {
+                file.Write($"{nameAction};;");
+
+                for (int j = 0; j < 200; j++)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        FindTime(num, f, time, j);
+                    }
+                    int average = FindAverage(time);
+
+                    file.Write($"{average};");
+                    time.Clear();
+                }
+
+                file.WriteLine();
+                file.Close();
+            }
+        }
+
+        static void OutputDataArray(int[] arr, Action<int[], int> f, string nameAction)
+        {
+            List<int> time = new List<int>();
+
+            using (StreamWriter file = new StreamWriter("Time.txt", true))
+            {
+                file.Write($"{nameAction};;");
+
+                for (int j = 0; j < 1000; j++)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        FindTimeArr(arr, f, time, j);
+                    }
+                    int average = FindAverage(time);
+
+                    file.Write($"{average};");
+                    time.Clear();
+                }
+
+                file.WriteLine();
+                file.Close();
+            }
+        }
+
+        public static void FindTime(int num, Action<int, int> f, List<int> time, int count)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            f(num,count);
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}", ts.Ticks);
+            Console.WriteLine(elapsedTime);
+
+            time.Add(Convert.ToInt32(elapsedTime));
+        }
+
+        public static void FindTimeArr(int[] arr, Action<int[], int> f, List<int> time, int count)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            f(arr, count);
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}", ts.Ticks);
+            Console.WriteLine(elapsedTime);
+
+            time.Add(Convert.ToInt32(elapsedTime));
+        }
+        static int FindAverage(List<int> time)
+        {
+            int sum = 0;
+            for (int i = 0; i < time.Count; i++)
+            {
+                sum += time[i];
+            }
+            return sum / time.Count;
+        }
+
+        
+    }
+}
